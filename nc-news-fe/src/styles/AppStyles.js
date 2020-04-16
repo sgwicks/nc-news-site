42175b7ee23f00599d0a925b1d0b9cde233b8b40
sgwicks <samgwicks@gmail.com>
2020-04-16 16:57:12 +0100

    Fixed issue with Main content not stretching to fit container

--- nc-news-fe/src/styles/AppStyles.js
+++ nc-news-fe/src/styles/AppStyles.js
@@ -1,21 +1,22 @@
 import styled from 'styled-components';
 import { Router } from '@reach/router';
 
-export const StyledRouter = styled(Router)`
-  grid-area: AppMain;
-  display: flex;
-`;
-
 export const StyledApp = styled.div`
   text-align: center;
   display: grid;
-  grid-template-columns: 1fr 5fr 1fr;
+  grid-template-columns: 1fr 4fr 1fr;
   grid-template-rows: auto;
   grid-template-areas:
-    '. AppHeader . '
+    '. AppHeader .'
     '. AppNav .'
     '. AppMain .'
     '. AppFooter .';
+`;
+
+export const StyledRouter = styled(Router)`
+  grid-area: AppMain;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const StyledFooter = styled.footer`
