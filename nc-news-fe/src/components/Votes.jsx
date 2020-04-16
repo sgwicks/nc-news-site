f5ec82998e3b9cb52bfbff0f72fe92441c6710cd
sgwicks <samgwicks@gmail.com>
2020-04-16 13:24:53 +0100

    Added error handling to votes component

--- nc-news-fe/src/components/Votes.jsx
+++ nc-news-fe/src/components/Votes.jsx
@@ -3,39 +3,45 @@ import * as api from '../utils/api';
 
 class Votes extends Component {
   state = {
-    votes: null,
-    voted: 0
+    voted: 0,
+    isError: null
   };
 
-  componentDidMount() {
-    this.setState({ votes: this.props.votes });
-  }
-
   render() {
-    const { votes, voted } = this.state;
+    const { voted, isError } = this.state;
+    const { votes } = this.props;
     return (
       <section>
         Votes:
         <button onClick={() => this.handleClick(-1)} disabled={voted < 0}>
           -
         </button>
-        {votes}
+        {votes + voted}
         <button onClick={() => this.handleClick(1)} disabled={voted > 0}>
           +
         </button>
+        {isError && (
+          <p style={{ color: 'red' }}>
+            Something went wrong. Please try again later
+          </p>
+        )}
       </section>
     );
   }
 
-  handleClick = (num) => {
+  handleClick = async (num) => {
     const { type, id } = this.props;
     this.setState((currentState) => {
       return {
-        votes: currentState.votes + num,
+        isError: null,
         voted: currentState.voted + num
       };
     });
-    api.patchVotesById(type, id, num);
+    try {
+      await api.patchVotesById(type, id, num);
+    } catch (err) {
+      this.setState({ isError: true, voted: 0 });
+    }
   };
 }
 
